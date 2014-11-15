1ca5db4833eaf7e75cf9a35c862051905d6c7628
mojieyao <guillaume@mohr.fr>
2014-11-15 22:58:46 +0100

    fixed several typos in the comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
 ## These two functions build a special matrix object that caches its inverse
 ## so it does not have to compute it more than once.
 
-## This function create a special matrix object that is accessible through a list of
-## four function : get, set, setinverse and getinverse.
+## This function create a special matrix object that contains its inverse once it is
+## computed for the first time. The object is accessible through a list of
+## four functions : get, set, setinverse and getinverse.
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL    #the variable that will contain the inverse matrix, at creation it is NULL  
     set <- function(y) {    #function that can change the matrix
@@ -21,17 +22,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function returns the inverse of a special matrix either 
 ##  (i) by returning the inverse that is already cached in the special matrix
 ##      structure ; or
-## (ii) by computing the inverse in case it has not been computerd befor. 
+## (ii) by computing the inverse in case it has not been computed before. 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    m <- x$getinverse()
-    if(!is.null(m)) {   #the inverse has alread been computed, so we do not need to do it again
-        message("getting cached data")
-        return(m)
+    
+    #First, we check that the inverse is not already chached in the matrix object
+    i <- x$getinverse()
+    if(!is.null(i)) {   
+        message("getting cached data") #the inverse has alread been computed, so we do not need to do it again
+        return(i)
     }
+    
     #if we arrive to this point, it means the inverse has not yet been computed
     data <- x$get()    #we get the matrix values
-    m <- solve(data, ...)   #compute the inverse matrix
-    x$setinverse(m)    #set the inverse in the special matrix object
-    m
+    i <- solve(data, ...)   #compute the inverse matrix
+    x$setinverse(i)    #set the inverse in the special matrix object
+    i    #return the inverse
 }
